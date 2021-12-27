89abfd5af8050207934867c9d3a8250c7a168398
L II <lii@Ls-MacBook-Pro.lan>
2021-12-26 19:05:42 -0600

    added onClick and prevent page from refreshing after submit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState(['Take dogs for a walk', 'Take out the garbage', 'Complete CleverProgrammer exercises']);
+  const [input, setInput] = useState('');
+  console.log('🚀', input);
+
+  const addTodo = (event) => {
+    // this will fire off when button is clicked.
+    event.preventDefault();
+    console.log('🎯', 'YAY!!')
+    setTodos([...todos, input]);
+    setInput('');  // clears input after entering todo.
+  }
 
   return (
     <div className="App">
         <h1>Hello world!</h1>
-        <input />
-        <button>Add ToDo</button>
+        <form>
+        <input value={input} onChange={event => setInput(event.target.value)}/>
+        <button type='submit' onClick={addTodo}>Add ToDo</button>
+        </form>
 
         <ul>
           {todos.map(todo => (
